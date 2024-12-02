b43a5d4953a54ea2fa8fcfdb8bbbc29935565005
Nadim Bouassida <ndaemm@gmail.com>
2024-12-02 13:09:53 +0100

    fixed the problem with positionning the search bar  befor and after collapse using a js script "moveSearchBar.js"

---
+++ public/scripts/moveSearchBar.js
@@ -0,0 +1,14 @@
+// Get the search bar element
+const searchBar = document.querySelector(".search-bar");
+const navbarBrand = document.querySelector(".navbar-brand");
+const navbarLeft = document.querySelector(".navbar-left");
+
+window.addEventListener("resize", () => {
+  // Check if the window width is less than 768px
+  if (window.innerWidth < 768) {
+    navbarBrand.after(searchBar);
+  }
+  else{
+    navbarLeft.after(searchBar);
+  }
+});
