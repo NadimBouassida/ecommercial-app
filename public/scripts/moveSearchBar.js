3dbad18da95f0cc69fc2b44b16aa86f8f34e1026
Nadim Bouassida <ndaemm@gmail.com>
2024-12-04 07:10:12 +0100

    modified moveSearchBar.js script to put the searchbar in the right place on launch without resize

--- public/scripts/moveSearchBar.js
+++ public/scripts/moveSearchBar.js
@@ -3,6 +3,10 @@ const searchBar = document.querySelector(".search-bar");
 const navbarBrand = document.querySelector(".navbar-brand");
 const navbarLeft = document.querySelector(".navbar-left");
 
+if (window.innerWidth < 768){
+  navbarBrand.after(searchBar);
+}
+
 window.addEventListener("resize", () => {
   // Check if the window width is less than 768px
   if (window.innerWidth < 768) {
